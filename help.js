6f34019cf822ae879f135f8c04b975f8f6343554
GHOSTSIRAN <beyramisepehr87@gmail.com>
2025-05-14 02:57:20 +0330

    Add files via upload

---
+++ help.js
@@ -0,0 +1,18 @@
+//Credit: Senz
+
+const fs = require('fs')
+const config = JSON.parse(fs.readFileSync(`./config.json`))
+
+exports.start = async(lol, name) => {
+    text = `Welcome To RexonC2 DDoS Bot. if you want use it you must join @rexonc2\n/help see my commands`
+    await lol.replyWithMarkdown(text, { disable_web_page_preview: true })
+}
+
+exports.help = async(lol, name) => {
+    text = `This is beta bot not powerfull\n/methods show my methods\n/attack how to attack`
+    await lol.replyWithMarkdown(text, { disable_web_page_preview: true })
+}
+
+exports.messageError = async(lol) => {
+    await lol.reply(`Error! Please report to the [${config.owner}](${config.ownerLink}) about this`, { parse_mode: "Markdown", disable_web_page_preview: true })
+}
